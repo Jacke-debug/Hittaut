fff554406f72be580b4a55a3d8956863c6298a82
jakob_laurell <jakob.laurell@gmail.com>
2020-06-22 14:22:58 +0200

    helper loops through all hittaut areas and calls the hittaut.py for each area. Hittaut.oy tries to find the dates for winner announcements but some more work is needed here.

---
+++ helper.py
@@ -0,0 +1,34 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import datetime
+import hittaut
+import requests
+from requests.exceptions import HTTPError
+
+# "manadsvinnare"
+my_url = "https://www.orientering.se/provapaaktiviteter/hittaut/"
+
+# opening up connection, grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+
+# grabs each product
+pageMenu = page_soup.findAll("li", {"class":"secondary-topmenu__submenu-item"})
+
+basePage = "https://www.orientering.se/"
+for item in pageMenu:
+    webPage = basePage + item.a["href"] + "manadsvinnare/"
+    # TODO: Add check if page exists or if it should be something else at the end..
+    try:
+        r = requests.get(webPage)
+        r.raise_for_status()
+    except HTTPError:
+        print('Could not download page')
+    else:
+        print(r.url, 'downloaded successfully')
+        hittaut.main(webPage)
+    
---
+++ hittaut.py
@@ -0,0 +1,68 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import datetime
+
+
+def findDate(winnerDatesStr):
+    winnerDatesStr = winnerDatesStr.lower()
+    # assumes month to be written as a word
+    month = monthParser(winnerDatesStr)
+    print(winnerDatesStr)
+    # extract numbers from the string
+    numbers = [int(s) for s in str.split(winnerDatesStr) if s.isdigit()]
+    if len(numbers) == 1:
+        # use the one number as a date
+        date = numbers[0]
+        month = 1
+    else:
+        # string contains more than one number
+        # may be a numbering of the dates i.e. 1. 2. 3.. 
+        # or that the month is written with a number
+        # or possibly both..
+        date = 1
+        if month == 0:
+            # month is written as a number not a word
+            # do something clever
+            month = 1
+        pass
+    x = datetime.datetime(2020, month, date)
+    return x
+
+def monthParser(winnerDatesStr):
+    month = 0
+    if "maj" in winnerDatesStr:
+        month = 5
+    elif "juni" in winnerDatesStr:
+        month = 6
+    elif "juli" in winnerDatesStr:
+        month = 7
+    elif "augusti" in winnerDatesStr:
+        month = 8
+    elif "september" in winnerDatesStr:
+        month = 9
+    return month
+
+def main(my_url): 
+    # opening up connection, grabbing the page
+    try:
+        uClient = uReq(my_url)
+    except:
+        exit
+
+    page_html = uClient.read()
+    uClient.close()
+    # html parsing
+    page_soup = soup(page_html, "html.parser")
+
+    # grabs each product
+    containers = page_soup.findAll("div", {"class":"rich-text"})
+    if containers is not None:
+        dates = []
+        for container in containers:
+            if container.ul is not None:
+                for content in container.ul:
+                    dates.append(findDate(content.text))
+        return dates
+    else:
+        return
+
