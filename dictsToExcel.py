1c46049c444f27713999ece48d0d53179f19d133
CodeAron <kodearon@gmail.com>
2020-08-17 16:10:59 +0200

    set to string, altough not very neat

--- dictsToExcel.py
+++ dictsToExcel.py
@@ -3,26 +3,32 @@ from xlsxwriter import Workbook
 def main(ortList):
 
     ordered_list=["name","draws"] #list object calls by index but dict object calls items randomly
+    header_names=["Namn","Datum"]
 
+    filename="hittaut - dragningar.xlsx"
     wb=Workbook("hittaut - dragningar.xlsx")
-    ws=wb.add_worksheet() #or leave it blank, default name is "Sheet 1"
+    ws=wb.add_worksheet("Dragningar") #or leave it blank, default name is "Sheet 1"
 
     first_row=0
     for header in ordered_list:
-        col=ordered_list.index(header) # we are keeping order.
-        ws.write(first_row,col,header) # we have written first row which is the header of worksheet also.
+        col=ordered_list.index(header) 
+        ws.write(first_row,col,header_names[col]) 
 
     row=1
     for ort in ortList:
         for _key,_value in ort.items():
             col=ordered_list.index(_key)
+            if type(_value)==set:
+                _value=str(_value)
             ws.write(row,col,_value)
         row+=1 #enter the next row
     wb.close()
 
+    return filename
+
 if __name__ == '__main__': # for testing/debugging
 
-    testList = [{'name': 'aröd', 'draws': '7/8,5/5,9/9'},
-    {'name': 'kode', 'draws': 'fr'}]
+    testList = [{'name': 'Aröd', 'draws': {'7/8','5/5','9/9'}},
+    {'name': 'kode', 'draws': set()}]
 
     main(testList)
