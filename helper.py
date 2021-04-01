f3bc1e5a506fdfa28593dfa8901ff78edcd154e4
CodeAron <67368318+CodeAron@users.noreply.github.com>
2021-04-01 19:20:41 +0200

    nCheckpts added to printout. Out of loaded pages instead of total.

--- helper.py
+++ helper.py
@@ -22,7 +22,8 @@ pageMenu = page_soup.find("ul",{"class":"secondary-topmenu__submenu regions"}).f
 
 basePage = "https://www.orientering.se/"
 nFailed=0 # number of "failed to load webpage"
-nEmpty=0 # number of "failed to find any dates"
+n_dates=0 # number of "Dates (successfully?) extracted"
+n_nCheckpts=0 # number of "nCheckpts (successfully?) extracted"
 listOfDicts=list()
 for item in pageMenu:
     webPage = basePage + item.a["href"]
@@ -33,15 +34,25 @@ for item in pageMenu:
         nFailed+=1
     else:
         listOfDicts.append(ortDict)
+        print('draws',end=' <> ')
         if len(ortDict["draws"]) == 0:
-            nEmpty+=1
-            print('No dates found.')
+            print('fail')
         else:
-            print('Dates (successfully?) extracted')
-        
+            n_dates+=1
+            print('success?')
+        print('nCheckpts',end=' <> ')
+        if ortDict["nCheckpts"] == -1:
+            print('fail')
+        else:
+            n_nCheckpts+=1
+            print('success?')
 
-print('Loaded pages: ', len(pageMenu)-nFailed,'/',len(pageMenu))
-print('Non-empty date sets: ', len(pageMenu)-nEmpty,'/',len(pageMenu))
+total = len(pageMenu)
+loaded = total-nFailed
+
+print('Loaded pages: ', loaded,'/',total)
+print('Date sets: ', n_dates,'/',loaded)
+print('nCheckpts sets: ', n_nCheckpts,'/',loaded)
 
 
 filename=dictsToExcel.main(listOfDicts)
