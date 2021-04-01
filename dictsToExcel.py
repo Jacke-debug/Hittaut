9a22a48a87de0fed966f6f5cb02d4abc7ed9390a
CodeAron <67368318+CodeAron@users.noreply.github.com>
2021-04-01 18:02:45 +0200

    Higher resolution on visualised sheet. Prints vertical lines to separate months. Prints a V where there is a price draw.

--- dictsToExcel.py
+++ dictsToExcel.py
@@ -19,17 +19,19 @@ def main(listofdicts):
         col=header_names.index(header) 
         ws_raw.write(first_row,col,header_names[col]) 
 
+    colummns_per_month = 10
     col=1
+    cfm_mon = wb.add_format({'left':1})
     for month in months:
-        ws_vis.write(first_row,col,month)
-        col+=1
+        ws_vis.write(first_row,col,month,cfm_mon)
+        col+=colummns_per_month
 
     ws_raw.set_column(0,0,15) # width for ort column
     ws_raw.set_column(4,4,20) # width for draws column
 
     ws_vis.set_column(0,0,15) # width for ort column
     #ws_vis.set_column(1,2,4) # width for empty columns
-    ws_vis.set_column(1,12,8) # width for month columns
+    ws_vis.set_column(1,12*colummns_per_month,0.9) # width for month columns
 
     row=1
     for dictEntry in listofdicts:
@@ -47,20 +49,49 @@ def main(listofdicts):
 
         # visualised worksheet
         ws_vis.write_url(row,0, dictEntry['url'], string=dictEntry['ort'])
-        start_month = dictEntry['start'] // 100
-        end_month = dictEntry['end'] // 100
-        cell_format = wb.add_format({'bg_color':'#dd235f'})
-        for monthCol in range(start_month,end_month):
-            ws_vis.write(row, monthCol, '', cell_format)
+        start_col = column_from_date(dictEntry['start'],colummns_per_month)
+        end_col = column_from_date(dictEntry['end'],colummns_per_month)
+        # print('start_col =',start_col)
+        # print('end_col =',end_col)
+        vinst_cols = []
+        for draw in dictEntry['draws']:
+            vinst_cols.append(column_from_date(draw,colummns_per_month))
+        cfm_on = wb.add_format({'bg_color':'#dd235f','font_color':'#FFFFFF'})
+        # cfm_off = wb.add_format({'bg_color':''})
+        cfm_on_mon = wb.add_format({'bg_color':'#dd235f','font_color':'#FFFFFF','left':1})
+        cfm_off_mon = wb.add_format({'left':1})
+        for col in range(1,12*colummns_per_month):
+            if col in vinst_cols:
+                text = 'V'
+            else:
+                text = ''
+            if start_col <= col and col <= end_col:
+                if (col-1) % colummns_per_month == 0:
+                    ws_vis.write(row, col, text, cfm_on_mon)
+                else:
+                    ws_vis.write(row, col, text, cfm_on)
+            else:
+                if (col-1) % colummns_per_month == 0:
+                    ws_vis.write(row, col, text, cfm_off_mon)
 
 
         row+=1 #enter the next row
 
-    
-
     wb.close()
 
     return filename
+
+def column_from_date(datenumber,colummns_per_month):
+    # datenumber: last two digits for day, first two (or one) for month
+    month = datenumber // 100
+    day = datenumber % 100
+    day_col = int(colummns_per_month*(day/30))
+    if day_col>30:
+        day_col=30
+    elif day_col<1:
+        day_col=1
+    colnumber = (month-1)*colummns_per_month + day_col
+    return colnumber
 
 if __name__ == '__main__': # for testing/debugging
 
@@ -68,6 +99,6 @@ if __name__ == '__main__': # for testing/debugging
     # {'ort': 'kode','url':'kode.se', 'draws': list(), 'dates':'','nCheckpts': '60'},
     # {'ort': 'Kungälv', 'url':'kungalv.se', 'dates': '10 Apr - 11 Oct', 'nCheckpts': '60', 'draws': ['-/8', '-/5', '-/10']}]
 
-    testList = [{'ort': 'Kalmar', 'url': 'https://www.orientering.se/provapaaktiviteter/hittaut/kalmar/', 'start': 501, 'end': 1030, 'nCheckpts': '130', 'draws': [1030]}]
+    testList = [{'ort': 'Kalmar', 'url': 'https://www.orientering.se/provapaaktiviteter/hittaut/kalmar/', 'start': 501, 'end': 1030, 'nCheckpts': '130', 'draws': [815,1030]}]
 
     main(testList)
